4094a928ae44c04a6c74015dd550ce84c3391bfe
gudzitska-darina <gudzitska.darina@gmail.com>
2020-10-28 18:37:40 +0200

    finish lab1

---
+++ lab1/z1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Math;
+
+       
+Console.WriteLine("Write 3 numbers ");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+double z = double.Parse(Console.ReadLine());
+double a=0;
+double b=0;
+if( Pow(x,3)+5*Pow(y,-z)+Pow(z,2) >= 0 || a!=0 || y+z/a!=0 || x+(a/(y+z/a))!=0)
+{
+    a = Sqrt(Pow(x,3)+5*Pow(y,-z)+Pow(z,2)) - Pow(Abs(Sin(x)-Cos(y)),1/3);
+    b = a/(x+(a/(y+z/a)));
+    Console.WriteLine("a:{0}", a);
+    Console.WriteLine("b:{0}", b);
+
+}
+else
+{
+    Console.WriteLine("Doesn`t correspond to ODZ!");
+}
+        
---
+++ lab1/z2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Введите век ");
+                byte x = byte.Parse(Console.ReadLine());
+                const int d = 13;
+                int c = x - 1;
+                int y = 0;
+                int m = 0;
+                int mm = 0;
+                int res = 0;
+                int yy = 0;
+                while (y < 100)
+                {
+                    m = 1;
+                    int cc = x - 1;
+                    while (m <= 12)
+                    {
+                    if (m < 3)
+                    {
+                        yy = y - 1;
+                        mm = m + 10;
+                    }
+                    else
+                    {
+                        mm = m - 2;
+                        yy = y;
+                    }
+                        if (yy < 0)
+                        {
+                            yy = 100 + yy;
+                            cc = cc - 1; 
+                        }
+                        int a = ((13 * mm - 1)/5 + d + yy + (yy / 4) + (cc / 4) - 2 * cc + 777);
+                        if (a % 7 == 5)
+                        {
+                            res++;
+                        }
+                        m++;
+                    }
+                y++;
+                }
+                Console.WriteLine(res);
+                Console.ReadKey();
+        }
+    }
+}
